c5f77a0bfed25eaebc73edc3743def36111dba22
Pablo Barrios <barrios.p@outlook.com>
2021-09-20 13:55:52 -0300

    cambios para la entrega

--- js/main.js
+++ js/main.js
@@ -19,6 +19,52 @@ $(document).ready( () => {
             document.getElementsByTagName("html")[0].style.overflow = "auto";
             flag = 1
         }
-    
     })
+    
+    let comActual = 1
+    
+    $('#com-sig').click( () => {
+
+        if (comActual == 1) {
+            $('#com-1').css("display", "none")
+            $('#com-2').css("display", "inline")
+            $('#com-3').css("display", "none")
+            comActual = 2
+        }
+        else if (comActual == 2) {
+            $('#com-1').css("display", "none")
+            $('#com-2').css("display", "none")
+            $('#com-3').css("display", "inline")
+            comActual = 3
+        }
+        else if (comActual == 3) {
+            $('#com-1').css("display", "inline")
+            $('#com-2').css("display", "none")
+            $('#com-3').css("display", "none")
+            comActual = 1
+        }
+    })
+
+    $('#com-ant').click( () => {
+
+        if (comActual == 1) {
+            $('#com-1').css("display", "none")
+            $('#com-2').css("display", "none")
+            $('#com-3').css("display", "inline")
+            comActual = 3
+        }
+        else if (comActual == 2) {
+            $('#com-1').css("display", "inline")
+            $('#com-2').css("display", "none")
+            $('#com-3').css("display", "none")
+            comActual = 1
+        }
+        else if (comActual == 3) {
+            $('#com-1').css("display", "none")
+            $('#com-2').css("display", "inline")
+            $('#com-3').css("display", "none")
+            comActual = 2
+        }
+    })
+
 })
